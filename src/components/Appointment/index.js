ab2a7bccf3c36112b79f00e30ea5ab13919f4ba1
Vanessa Power <vanessa.l.power@gmail.com>
2022-12-13 19:05:05 -0800

    implementing usevisualmode hook on appointments componenent

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,19 +2,35 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 import "./styles.scss";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
+      )}
+      {mode === CREATE && (
+        <Form
+          student=""
+          //interviewer={3}
+          interviewers={[]}
+          //onSave={action("onSave")}
+          onCancel={() => transition(EMPTY)}
+        />
       )}
     </article>
   );
